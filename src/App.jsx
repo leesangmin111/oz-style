649426de5560d94e3ece5b3e8a865880d07349c8
isangmin111 <sangmin9800@naver.com>
2025-02-21 13:28:07 +0900

    styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
-import "./App.scss";
+import React from "react";
+import styled from "styled-components";
 import { contents } from "./assets/data/data";
 import Content from "./components/Content";
 import Header from "./components/Header";
 
+const ContentsContainer = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 20px;
+  padding: 20px 40px;
+`;
+
 function App() {
   return (
-    <main>
+    <>
       <Header />
-      <section>
+      <ContentsContainer>
         {contents.map((el) => (
           <Content key={el.id} content={el} />
         ))}
-      </section>
-    </main>
+      </ContentsContainer>
+    </>
   );
 }
 
